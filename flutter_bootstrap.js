3cc8d37d1b5f6b8b6716a8d514c19c815fa62b94
damian-tochi <damian-tochi@users.noreply.github.com>
2025-04-23 06:30:20 +0000

    deploy: d2fe78fa730d7847b4f8b41bd6a0bd9ae4da669a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "659285353"
+    serviceWorkerVersion: "1518427903"
   }
 });
